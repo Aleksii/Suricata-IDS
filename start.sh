1efceb572231efa2a424a71bc9295fe10504c245
Aleksii <alexmisnik91@gmail.com>
2018-01-11 21:28:43 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-exec /usr/bin/suricata -c /etc/suricata/suricata.yaml -i eth0 --init-errors-fatal
+exec /usr/bin/suricata -c /etc/suricata/suricata.yaml -i eth0 --init-errors-fatal &
